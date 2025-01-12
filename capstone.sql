e80f56968499776f65944aacb0a25dde3db1f7b2
Emmanuel Martins <Emmaugomartins@gmail.com>
2025-01-12 21:35:57 +0100

    Add files via upload
    1. Category Performance:
    
    The total profit is 112K.
    
    Electronics made the highest profit of 44K, followed by Clothing with 35K, and Furniture with 33K.
    
    The sales targets show Electronics is leading (39.92%), while Clothing and Furniture are almost equal, at 30.49% and 29.59%.
    
    
    
    2. Sub-Category Breakdown:
    
    Quantity Sold: Sarees, Handkerchiefs, and Socks are the most sold items (over 750 units each).
    
    Profit by Sub-Category: Bookcases (16K), Printers (15K), and Phones (14K) generate the highest profits, while items like Skirts and Leggings generate the least profit (1K each).
    
    
    
    3. State-Wise Performance:
    
    Madhya Pradesh has the highest order count (101), followed by Maharashtra (90). Other states have much lower counts.
    
    The profit per quantity is highest when fewer items are sold, suggesting premium items drive profit.
    
    In summary:
    
    Electronics is the top-performing category.
    
    Some sub-categories, like Sarees and Handkerchiefs, sell in high quantities, but premium items like Bookcases and Printers are more profitable.
    
    Madhya Pradesh leads in order volume, and high-value items significantly impact profits.

---
+++ capstone.sql
@@ -0,0 +1,61 @@
+-- TOTAL CAPSTONE PROFIT
+use capstone;
+select sum(profit) as 
+total_profit
+from `order details`;
+
+-- PROFIT PER CATEGORY
+select Category,
+sum(profit) as total_profit
+from `order details`
+group by Category;
+
+
+-- QUANTITY PER CATEGORY
+select category, sum(Quantity) as
+Total_Quantity
+from `order details`
+group by Category;
+
+-- TOTAL ORDERS PER CITY
+select City, count(`Order ID`) as
+Total_sales
+from`list of orders`
+group by city;
+
+-- AVERAGE PROFIT PER QUANTITY OF CATEGORY
+select category, sum(profit)/ sum(Quantity)  as
+Avg_Profit_Per_Quantity
+from`order details`
+group by category;
+
+-- SUM OF QUANTITY BY SUB_CATEGORY
+select `Sub Category`,
+sum(Quantity) as Total_Quantity
+from`order details`
+group by `Sub Category`
+order by Total_Quantity desc;
+
+
+-- SUM OF PROFIT BY SUB_CATEGORY
+select `Sub Category`,
+sum(profit) as Total_profit
+from `order details`
+group by `Sub Category`
+order by Total_profit desc;
+
+-- TOP PERFORMING SUB_CATEGORIES
+select `Sub Category`,
+sum(profit) as Total_profit
+from `order details`
+group by`Sub Category`
+order by Total_profit desc
+limit 10;
+
+-- LEAST PERFORMING CITY
+select city,
+count(`Order ID`) as Total_profit
+from `list of orders`
+group by city
+order by Total_profit asc
+limit 10;
